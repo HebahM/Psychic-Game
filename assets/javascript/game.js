5e4b83401ccdc78611fe85cf4d005e813fd08541
HebahM <56455544+HebahM@users.noreply.github.com>
2019-12-14 23:14:25 -0600

    completed js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,40 @@
 var numWins = 0;
 var numLosses = 0;
+var guessesLeft = 9;
+var yourGuesses = [];
+var chars = "abcdefghiklmnopqrstuvwxyz";
+var randomstring = '';
+var rnum = Math.floor(Math.random() * chars.length);
+randomstring = chars.substring(rnum, rnum + 1);
 
 
-document.onkeyup = function(event) {
-    var chars = "abcdefghiklmnopqrstuvwxyz";
-    var randomstring = '';
-    var userGuess = event.key
-	for (var i=0; i<1; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    if (userGuess.toLowerCase === randomstring) {
-        numWins++;
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    
+    yourGuesses.push(userGuess);
+    if (userGuess !== randomstring) {
+        if (guessesLeft > 0) {
+            guessesLeft--;
+        }
     }
     else {
-        numLosses++;
+        numWins++;
+        yourGuesses = [];
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring = chars.substring(rnum, rnum + 1);
+        guessesLeft = 9;
+
     }
-    document.getElementById("wins").textContent = "Wins: " + numWins;
-    document.getElementById("losses").textContent = "Losses: " + numLosses;
+    if (guessesLeft==0) {
+        numLosses++;
+        yourGuesses = [];
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring = chars.substring(rnum, rnum + 1);
+        guessesLeft = 9;
+    }
+    
+    console.log("number of guesses left:")
+    console.log(guessesLeft)
     console.log("computer's letter:")
     console.log(randomstring)
     console.log("user's input:")
@@ -26,7 +43,44 @@ document.onkeyup = function(event) {
     console.log(numWins)
     console.log("losses")
     console.log(numLosses)
+    document.getElementById("wins").textContent = "Wins: " + numWins;
+    document.getElementById("losses").textContent = "Losses: " + numLosses;
+    document.getElementById("guesses").textContent = "Guesses left: " + guessesLeft;
+    document.getElementById("guessedLetters").textContent = "Your guesses so far: " + yourGuesses;
 }
+
+
+/*function clear() {
+    guessesLeft = 0;
+    yourGuesses = [];
+}
+/*if (numWins++ || numLosses++) {
+    console.log("clear")
+    clear();
+}
+
+/* for (var i=0; i<1; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum,rnum+1);
+}
+if (userGuess.toLowerCase === randomstring) {
+    numWins++;
+}
+else {
+    numLosses++;
+}
+document.getElementById("wins").textContent = "Wins: " + numWins;
+document.getElementById("losses").textContent = "Losses: " + numLosses;
+console.log("computer's letter:")
+console.log(randomstring)
+console.log("user's input:")
+console.log(userGuess)
+console.log("wins:")
+console.log(numWins)
+console.log("losses")
+console.log(numLosses)
+} */
+
 
 
 
@@ -42,4 +96,4 @@ document.onkeyup = function(event) {
 // else, add userInput to yourGuesses
 // try 9 more times???
 // after 10 tries, losses ++
-// start over
+// start over */
